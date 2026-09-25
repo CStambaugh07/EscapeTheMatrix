75efde677557e0a4569d0b3478f4eb3a6e3a384d
stambaugh <stambaugh@etown.edu>
2026-09-24 20:41:27 -0400

    Initial Commit

---
+++ src/escapeTheMatrix/EscapeTheMatrix.java
@@ -0,0 +1,15 @@
+package escapeTheMatrix;
+
+import javax.swing.*;
+
+public class EscapeTheMatrix {
+
+	
+	public static void main (String[] args) {
+		JFrame frame = new JFrame("Escape the Matrix");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module escapeTheMatrix {
+	requires java.desktop;
+}
